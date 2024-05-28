d40cad6c442efd59df266866d9adbe31db4e0ffa
2221403 <161707428+2221403@users.noreply.github.com>
2024-05-28 18:51:36 +0100

    cenas i guess

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
-use App\Http\Controllers\MovieController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home')->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('movies', [MovieController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
