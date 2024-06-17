34fdef3fe0ba1fc2d73a494dd35c6d3c47040daa
2221403 <161707428+2221403@users.noreply.github.com>
2024-06-17 18:28:37 +0100

    home pt->en

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="relative py-12 bg-cover bg-center" style="background-image: url('{{ asset('img/background.jpg') }}'); height: 93vh;">
         <div class="flex items-center justify-center h-full">
-            <h1 class="text-4xl font-black text-darkblue">Bem-vindo ao CineMagic</h1>
+            <h1 class="text-4xl font-black text-darkblue">Welcome to CineMagic</h1>
         </div>
     </div>
 </x-app-layout>
